bd3dbe9d7aae59a99ad89c20add17403c78aaf43
Riku Pepponen <riku.pepponen@gmail.com>
2017-07-08 15:00:41 +0300

    Hide actionbar from AboutActivity

--- app/src/main/java/exxxample/ti3rules/AboutActivity.java
+++ app/src/main/java/exxxample/ti3rules/AboutActivity.java
@@ -14,5 +14,8 @@ public class AboutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        // Hide the action bar
+        getSupportActionBar().hide();
     }
 }
