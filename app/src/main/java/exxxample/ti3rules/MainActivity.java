82dd016dba2394d29f840066d9ab3baa787e277e
Riku Pepponen <riku.pepponen@gmail.com>
2017-07-08 14:45:55 +0300

    MainActivity button listeners

--- app/src/main/java/exxxample/ti3rules/MainActivity.java
+++ app/src/main/java/exxxample/ti3rules/MainActivity.java
@@ -1,7 +1,9 @@
 package exxxample.ti3rules;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,8 +17,34 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Basic Rules button listener
         mButtonBasicRules = (Button)findViewById(R.id.buttonBasicRules);
+        mButtonBasicRules.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(MainActivity.this, BasicRulesActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // Lore button listener
         mButtonLore = (Button)findViewById(R.id.buttonLore);
+        mButtonLore.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(MainActivity.this, LoreActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // About button listener
         mButtonAbout = (Button)findViewById(R.id.buttonAbout);
+        mButtonAbout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(MainActivity.this, AboutActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
