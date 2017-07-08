438b6e2b60969440d78c67a47f247788a0aad0ce
Riku Pepponen <riku.pepponen@gmail.com>
2017-07-08 13:57:52 +0300

    New files for the AboutActivity

---
+++ app/src/main/java/AboutActivity.java
@@ -0,0 +1,9 @@
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Riku Pepponen on 8.7.2017.
+ * (riku.pepponen@gmail.com)
+ */
+
+public class AboutActivity extends AppCompatActivity {
+}
