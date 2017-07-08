850b000a2155552e6ba53b6453fc5738df1649c8
Riku Pepponen <riku.pepponen@gmail.com>
2017-07-08 13:56:08 +0300

    MainActivity's button declarations

--- app/src/main/java/exxxample/ti3rules/MainActivity.java
+++ app/src/main/java/exxxample/ti3rules/MainActivity.java
@@ -2,12 +2,21 @@ package exxxample.ti3rules;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button mButtonBasicRules;
+    Button mButtonLore;
+    Button mButtonAbout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mButtonBasicRules = (Button)findViewById(R.id.buttonBasicRules);
+        mButtonLore = (Button)findViewById(R.id.buttonLore);
+        mButtonAbout = (Button)findViewById(R.id.buttonAbout);
     }
 }
