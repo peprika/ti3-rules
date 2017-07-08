3af9e170882d7ca01269a791d37d2a285f884edc
Riku Pepponen <riku.pepponen@gmail.com>
2017-07-08 14:50:42 +0300

    New files for LoreActivity and BasicRulesActivity

---
+++ app/src/main/java/exxxample/ti3rules/BasicRulesActivity.java
@@ -0,0 +1,17 @@
+package exxxample.ti3rules;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Riku Pepponen on 8.7.2017.
+ * (riku.pepponen@gmail.com)
+ */
+
+public class BasicRulesActivity extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_basic_rules);
+    }
+}
