a85bd3e252dce4a930b5492da91913980efc9bf4
Riku Pepponen <riku.pepponen@gmail.com>
2017-07-08 14:00:20 +0300

    AboutActivity refactored and onCreate initialized

--- app/src/main/java/AboutActivity.java
+++
@@ -1,9 +0,0 @@
-import android.support.v7.app.AppCompatActivity;
-
-/**
- * Created by Riku Pepponen on 8.7.2017.
- * (riku.pepponen@gmail.com)
- */
-
-public class AboutActivity extends AppCompatActivity {
-}
---
+++ app/src/main/java/exxxample/ti3rules/AboutActivity.java
@@ -0,0 +1,17 @@
+package exxxample.ti3rules;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Riku Pepponen on 8.7.2017.
+ * (riku.pepponen@gmail.com)
+ */
+
+public class AboutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+}
