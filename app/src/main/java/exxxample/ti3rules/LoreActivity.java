cfe5652becb3852584a3cc89057aad0b04ab936d
Riku Pepponen <riku.pepponen@gmail.com>
2017-07-09 16:52:13 +0300

    Listview basics

--- app/src/main/java/exxxample/ti3rules/LoreActivity.java
+++ app/src/main/java/exxxample/ti3rules/LoreActivity.java
@@ -1,22 +1,31 @@
 package exxxample.ti3rules;
 
+import android.app.Activity;
 import android.app.ListActivity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import static android.R.id.list;
 
 /**
  * Created by Riku Pepponen on 8.7.2017.
  * (riku.pepponen@gmail.com)
  */
 
-public class LoreActivity extends AppCompatActivity {
+public class LoreActivity extends Activity {
 
-    TextView testTextView;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lore);
+
+        final ListView mListView = (ListView) findViewById(R.id.list);
 
         // Create Sol/Sardakk Races, input the name to textview (all we need for now)
         Data.Race sol = new Data.Race();
@@ -24,11 +33,27 @@ public class LoreActivity extends AppCompatActivity {
         Data.Race sardakk = new Data.Race();
         sardakk.raceName = getResources().getString(R.string.sardakk);
 
-        testTextView = (TextView)findViewById(R.id.textView);
-        testTextView.setText(sol.raceName);
-
         // Create an array of Race objects
         Data.Race[] racesArray = new Data.Race[10];
+        for (int i = 0; i < 10; i++) {
+            racesArray[i] = sol;
+        }
+
+        // ListView adapter
+        final ArrayAdapter<Data.Race> adapter = new ArrayAdapter<>(this,
+                R.layout.lore_list_view_single_item, R.id.listViewItem, racesArray);
+        mListView.setAdapter(adapter);
+
+        // ListView click listener
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                Toast.makeText(getApplicationContext(),
+                        "Click ListItem Number " + position, Toast.LENGTH_LONG)
+                        .show();
+            }
+        });
     }
 
 }
