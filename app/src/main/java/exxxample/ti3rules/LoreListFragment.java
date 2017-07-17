19f1d38c9e627056401ad36f26e499bedf128775
Riku Pepponen <riku.pepponen@gmail.com>
2017-07-17 14:01:04 +0300

    Simplified the race naming convention

--- app/src/main/java/exxxample/ti3rules/LoreListFragment.java
+++ app/src/main/java/exxxample/ti3rules/LoreListFragment.java
@@ -38,56 +38,59 @@ public class LoreListFragment extends ListFragment implements View.OnClickListen
 
 
     public Data.Race[] buildRaces() {
+
+        String [] raceNames = getResources().getStringArray(R.array.RaceNames);
+
         // Let's build the races!
         // #01: The Barony of Letnev
         Data.Race letnev = new Data.Race();
         letnev.raceId = 1;
-        letnev.raceName = getResources().getString(R.string.letnev);
+        letnev.raceName = raceNames[0];
 
         // #02: The Emirates of Hacan
         Data.Race hacan = new Data.Race();
         hacan.raceId = 2;
-        hacan.raceName = getResources().getString(R.string.hacan);
+        hacan.raceName = raceNames[1];
 
         // #03: Federation of Sol
         Data.Race sol = new Data.Race();
         sol.raceId = 3;
-        sol.raceName = getResources().getString(R.string.sol);
+        sol.raceName = raceNames[2];
 
         // #04: The L1Z1X Mindnet
         Data.Race lizix = new Data.Race();
         lizix.raceId = 4;
-        lizix.raceName = getResources().getString(R.string.lizix);
+        lizix.raceName = raceNames[3];
 
         // #05: The Mentak Coalition
         Data.Race mentak = new Data.Race();
         mentak.raceId = 5;
-        mentak.raceName = getResources().getString(R.string.mentak);
+        mentak.raceName = raceNames[4];
 
         // #06: The Naalu Collective
         Data.Race naalu = new Data.Race();
         naalu.raceId = 6;
-        naalu.raceName = getResources().getString(R.string.naalu);
+        naalu.raceName = raceNames[5];
 
         // #07: Sardakk N'orr
         Data.Race sardakk = new Data.Race();
         sardakk.raceId = 7;
-        sardakk.raceName = getResources().getString(R.string.sardakk);
+        sardakk.raceName = raceNames[6];
 
         // #08: The Universities of Jol-Nar
         Data.Race jolnar = new Data.Race();
         jolnar.raceId = 8;
-        jolnar.raceName = getResources().getString(R.string.jolnar);
+        jolnar.raceName = raceNames[7];
 
         // #09: The Xxcha Kingdom
         Data.Race xxcha = new Data.Race();
         xxcha.raceId = 9;
-        xxcha.raceName = getResources().getString(R.string.xxcha);
+        xxcha.raceName = raceNames[8];
 
         // #10: The Yssaril Tribes
         Data.Race yssaril = new Data.Race();
         yssaril.raceId = 10;
-        yssaril.raceName = getResources().getString(R.string.yssaril);
+        yssaril.raceName = raceNames[9];
 
         // Create an array of Race objects
         Data.Race[] racesArray = {letnev, hacan, sol, lizix, mentak, naalu, sardakk, jolnar, xxcha, yssaril};
