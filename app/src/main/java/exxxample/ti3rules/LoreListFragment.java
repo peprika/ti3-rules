edcb0bf965b130eb294a172e8e3aa30780bbc636
Riku Pepponen <riku.pepponen@gmail.com>
2017-07-17 12:09:15 +0300

    Cleaning up unnecessary imports

--- app/src/main/java/exxxample/ti3rules/LoreListFragment.java
+++ app/src/main/java/exxxample/ti3rules/LoreListFragment.java
@@ -5,16 +5,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-
 /**
  * Created by Riku Pepponen on 9.7.2017.
  * (riku.pepponen@gmail.com)
