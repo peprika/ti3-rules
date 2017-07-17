4832def189571c30379f6caec9c09c8a2cd5e665
Riku Pepponen <riku.pepponen@gmail.com>
2017-07-17 13:14:36 +0300

    Cleaning up LoreListFragment.java

--- app/src/main/java/exxxample/ti3rules/LoreListFragment.java
+++ app/src/main/java/exxxample/ti3rules/LoreListFragment.java
@@ -7,14 +7,13 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
 /**
  * Created by Riku Pepponen on 9.7.2017.
  * (riku.pepponen@gmail.com)
  */
 
 public class LoreListFragment extends ListFragment implements View.OnClickListener {
-
-    ListView mListView;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -25,24 +24,20 @@ public class LoreListFragment extends ListFragment implements View.OnClickListen
     public View onCreateView(LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.lore_list_fragment, container, false);
+
+        // Build the races objects
+        Data.Race[] racesArray = buildRaces();
+
+        // ListView adapter
+        ArrayAdapter<Data.Race> adapter = new ArrayAdapter<>(getActivity(),
+                R.layout.lore_list_view_single_item2, racesArray);
+        setListAdapter(adapter);
+
         return view;
     }
 
-    @Override
-    public void onActivityCreated(Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        ArrayAdapter adapter = ArrayAdapter.createFromResource(getActivity(),
-                R.array.RaceNames, R.layout.lore_list_view_single_item2);
-        setListAdapter(adapter);
-        //getListView().setOnItemClickListener(this);
-    }
 
-    /*@Override
-    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-        Toast.makeText(getActivity(), "Item: " + position, Toast.LENGTH_SHORT).show();
-    }*/
-
-    public void buildRaces() {
+    public Data.Race[] buildRaces() {
         // Let's build the races!
         // #01: The Barony of Letnev
         Data.Race letnev = new Data.Race();
@@ -97,23 +92,15 @@ public class LoreListFragment extends ListFragment implements View.OnClickListen
         // Create an array of Race objects
         Data.Race[] racesArray = {letnev, hacan, sol, lizix, mentak, naalu, sardakk, jolnar, xxcha, yssaril};
 
+        return racesArray;
     }
 
     @Override
     public void onClick(View v) {
-        // Somehting
+        // Something
     }
 
-        /*ListView mListView = getListView();
 
-        // ListView adapter
-        final ArrayAdapter<Data.Race> adapter = new ArrayAdapter<>(this,
-                R.layout.lore_list_view_single_item, R.id.listViewItem, racesArray);
-        mListView.setAdapter(adapter);
 
-        ArrayAdapter adapter = ArrayAdapter.createFromResource(getActivity(),
-                R.array.RaceNames, R.layout.lore_list_view_single_item);
-        setListAdapter(adapter);*/
-    //getListView().setOnItemClickListener(AdapterView.OnItemClickListener);
 }
 
